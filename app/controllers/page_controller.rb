a51dda1820be8b3a3880b68083776fa0f7048112
EmilyNg <ngemily13@gmail.com>
2016-11-27 21:44:53 -0500

    can do a basic query with a state, add sample partial

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,9 +1,22 @@
 class PageController < ApplicationController
+
   def index
-  	@states = States.all;
+  	if params[:q] == 'Alabama'
+  		@states = States.find_by_sql("SELECT* FROM state WHERE state.name = 'Alabama'");
+  		@arrests_2012 = Arrests_2012.find_by_sql("SELECT* FROM arrests_by_state_2012 WHERE arrests_by_state_2012.state = 'Alabama'");
+  		render "states_partial"
+  	end
+
   end
 
   def show
   	
   end
+
+  def update
+
+  end
+
+
+
 end
