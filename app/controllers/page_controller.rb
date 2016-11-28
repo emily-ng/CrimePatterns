dbb7da2799afd6aefff86f52446770e959fbf3ad
EmilyNg <ngemily13@gmail.com>
2016-11-27 22:02:59 -0500

    add simple search for a state

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,9 +1,9 @@
 class PageController < ApplicationController
 
   def index
-  	if params[:q] == 'Alabama'
-  		@states = States.find_by_sql("SELECT* FROM state WHERE state.name = 'Alabama'");
-  		@arrests_2012 = Arrests_2012.find_by_sql("SELECT* FROM arrests_by_state_2012 WHERE arrests_by_state_2012.state = 'Alabama'");
+  	if params[:q].present?
+  		@states = States.find_by_sql(["SELECT* FROM state WHERE state.name = ?", params[:q]]);
+  		@arrests_2012 = Arrests_2012.find_by_sql(["SELECT* FROM arrests_by_state_2012 WHERE arrests_by_state_2012.state = ?", params[:q]]);
   		render "states_partial"
   	end
 
