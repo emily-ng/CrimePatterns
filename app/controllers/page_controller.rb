86864e441491a82b003478ea3f4febb8c735230c
EmilyNg <ngemily13@gmail.com>
2016-11-29 16:45:47 -0500

    add dynamic chart using chartkick

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,7 +1,12 @@
 class PageController < ApplicationController
 
   def index
+
   	if params[:q].present?
+  		@result = {}
+	   	 States.where(name: params[:q]).each do |aa|
+	      @result[aa.year] = aa.est_pop
+	   	 end	
   		@states = States.find_by_sql(["SELECT* FROM state WHERE state.name = ?", params[:q]]);
   		@arrests_2012 = Arrests_2012.find_by_sql(["SELECT* FROM arrests_by_state_2012 WHERE arrests_by_state_2012.state = ?", params[:q]]);
   		render "states_partial"
