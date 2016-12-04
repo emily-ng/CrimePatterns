c2089ff507884c7ec3b60f151dd09be0b9238ea0
mem419 <michaela.murr@rutgers.edu>
2016-12-04 15:40:26 -0500

    state comparison
    comparing between 2 states now works !

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -6,6 +6,13 @@ class PageController < ApplicationController
   	@property_hash = {}
   	@drug_hash = {}
 
+    @violent_hash2 = {}
+    @property_hash2 = {}
+    @drug_hash2 = {}
+
+    @compare1_hash = {}
+    @compare2_hash = {}
+
   	#SINGLE STATE DATA FORM
   	if params[:q].present?
 	   	 States.where(name: params[:q]).each do |ss|
@@ -48,7 +55,138 @@ class PageController < ApplicationController
   	end
 
   	#COMPARISON FORM
-  	if params[:comaprison_crime].present?
+  	if params[:comparison_crime].present?
+      case params[:comparison_crime]
+      when "Violent"
+        States.where(name: params[:state1]).each do |ss|
+          if ss.year==2005
+            @a_2005 = Arrests_2005.find_by_sql(["SELECT * FROM arrests_by_state_2005 WHERE arrests_by_state_2005.age = 'Total all ages' AND arrests_by_state_2005.state = ?", ss.name]);
+            violent_crime = @a_2005.first.violent_crime.to_f
+          elsif ss.year==2007
+            @a_2007 = Arrests_2007.find_by_sql(["SELECT * FROM arrests_by_state_2007 WHERE arrests_by_state_2007.age = 'Total all ages' AND arrests_by_state_2007.state = ?", ss.name]);
+            violent_crime = @a_2007.first.violent_crime.to_f
+          elsif ss.year==2009
+            @a_2009 = Arrests_2009.find_by_sql(["SELECT * FROM arrests_by_state_2009 WHERE arrests_by_state_2009.age = 'Total all ages' AND arrests_by_state_2009.state = ?", ss.name]);
+            violent_crime = @a_2009.first.violent_crime.to_f
+          elsif ss.year==2011
+            @a_2011 = Arrests_2011.find_by_sql(["SELECT * FROM arrests_by_state_2011 WHERE arrests_by_state_2011.age = 'Total all ages' AND arrests_by_state_2011.state = ?", ss.name]);
+            violent_crime = @a_2011.first.violent_crime.to_f
+          elsif ss.year==2012
+            @a_2012 = Arrests_2012.find_by_sql(["SELECT * FROM arrests_by_state_2012 WHERE arrests_by_state_2012.age = 'Total all ages' AND arrests_by_state_2012.state = ?", ss.name]);
+            violent_crime = @a_2012.first.violent_crime.to_f
+          end 
+          percapita_violent1 = (violent_crime/ss.est_pop)*100000                    
+          #Make a hash per crime type in format: {year1=>value1, year2=>value2}
+          @compare1_hash[ss.year] = percapita_violent1
+        end
+        States.where(name: params[:state2]).each do |ss|
+          if ss.year==2005
+            @a_2005 = Arrests_2005.find_by_sql(["SELECT * FROM arrests_by_state_2005 WHERE arrests_by_state_2005.age = 'Total all ages' AND arrests_by_state_2005.state = ?", ss.name]);
+            violent_crime = @a_2005.first.violent_crime.to_f
+          elsif ss.year==2007
+            @a_2007 = Arrests_2007.find_by_sql(["SELECT * FROM arrests_by_state_2007 WHERE arrests_by_state_2007.age = 'Total all ages' AND arrests_by_state_2007.state = ?", ss.name]);
+            violent_crime = @a_2007.first.violent_crime.to_f
+          elsif ss.year==2009
+            @a_2009 = Arrests_2009.find_by_sql(["SELECT * FROM arrests_by_state_2009 WHERE arrests_by_state_2009.age = 'Total all ages' AND arrests_by_state_2009.state = ?", ss.name]);
+            violent_crime = @a_2009.first.violent_crime.to_f
+          elsif ss.year==2011
+            @a_2011 = Arrests_2011.find_by_sql(["SELECT * FROM arrests_by_state_2011 WHERE arrests_by_state_2011.age = 'Total all ages' AND arrests_by_state_2011.state = ?", ss.name]);
+            violent_crime = @a_2011.first.violent_crime.to_f
+          elsif ss.year==2012
+            @a_2012 = Arrests_2012.find_by_sql(["SELECT * FROM arrests_by_state_2012 WHERE arrests_by_state_2012.age = 'Total all ages' AND arrests_by_state_2012.state = ?", ss.name]);
+            violent_crime = @a_2012.first.violent_crime.to_f
+          end 
+          percapita_violent2 = (violent_crime/ss.est_pop)*100000                   
+          #Make a hash per crime type in format: {year1=>value1, year2=>value2}
+          @compare2_hash[ss.year] = percapita_violent2
+        end
+      when "Property"
+        States.where(name: params[:state1]).each do |ss|
+          if ss.year==2005
+            @a_2005 = Arrests_2005.find_by_sql(["SELECT * FROM arrests_by_state_2005 WHERE arrests_by_state_2005.age = 'Total all ages' AND arrests_by_state_2005.state = ?", ss.name]);
+            property_crime = @a_2005.first.property_crime.to_f
+          elsif ss.year==2007
+            @a_2007 = Arrests_2007.find_by_sql(["SELECT * FROM arrests_by_state_2007 WHERE arrests_by_state_2007.age = 'Total all ages' AND arrests_by_state_2007.state = ?", ss.name]);
+            property_crime = @a_2007.first.property_crime.to_f
+          elsif ss.year==2009
+            @a_2009 = Arrests_2009.find_by_sql(["SELECT * FROM arrests_by_state_2009 WHERE arrests_by_state_2009.age = 'Total all ages' AND arrests_by_state_2009.state = ?", ss.name]);
+            property_crime = @a_2009.first.property_crime.to_f
+          elsif ss.year==2011
+            @a_2011 = Arrests_2011.find_by_sql(["SELECT * FROM arrests_by_state_2011 WHERE arrests_by_state_2011.age = 'Total all ages' AND arrests_by_state_2011.state = ?", ss.name]);
+            property_crime = @a_2011.first.property_crime.to_f
+          elsif ss.year==2012
+            @a_2012 = Arrests_2012.find_by_sql(["SELECT * FROM arrests_by_state_2012 WHERE arrests_by_state_2012.age = 'Total all ages' AND arrests_by_state_2012.state = ?", ss.name]);
+            property_crime = @a_2012.first.property_crime.to_f
+          end 
+          percapita_property1 = (property_crime/ss.est_pop)*100000                     
+          #Make a hash per crime type in format: {year1=>value1, year2=>value2}
+          @compare1_hash[ss.year] = percapita_property1
+        end
+        States.where(name: params[:state2]).each do |ss|
+          if ss.year==2005
+            @a_2005 = Arrests_2005.find_by_sql(["SELECT * FROM arrests_by_state_2005 WHERE arrests_by_state_2005.age = 'Total all ages' AND arrests_by_state_2005.state = ?", ss.name]);
+            property_crime = @a_2005.first.property_crime.to_f
+          elsif ss.year==2007
+            @a_2007 = Arrests_2007.find_by_sql(["SELECT * FROM arrests_by_state_2007 WHERE arrests_by_state_2007.age = 'Total all ages' AND arrests_by_state_2007.state = ?", ss.name]);
+            property_crime = @a_2007.first.property_crime.to_f
+          elsif ss.year==2009
+            @a_2009 = Arrests_2009.find_by_sql(["SELECT * FROM arrests_by_state_2009 WHERE arrests_by_state_2009.age = 'Total all ages' AND arrests_by_state_2009.state = ?", ss.name]);
+            property_crime = @a_2009.first.property_crime.to_f
+          elsif ss.year==2011
+            @a_2011 = Arrests_2011.find_by_sql(["SELECT * FROM arrests_by_state_2011 WHERE arrests_by_state_2011.age = 'Total all ages' AND arrests_by_state_2011.state = ?", ss.name]);
+            property_crime = @a_2011.first.property_crime.to_f
+          elsif ss.year==2012
+            @a_2012 = Arrests_2012.find_by_sql(["SELECT * FROM arrests_by_state_2012 WHERE arrests_by_state_2012.age = 'Total all ages' AND arrests_by_state_2012.state = ?", ss.name]);
+            property_crime = @a_2012.first.property_crime.to_f
+          end 
+          percapita_property2 = (property_crime/ss.est_pop)*100000                      
+          #Make a hash per crime type in format: {year1=>value1, year2=>value2}
+          @compare2_hash[ss.year] = percapita_property2
+        end
+      when "Drug"
+        States.where(name: params[:state1]).each do |ss|
+          if ss.year==2005
+            @a_2005 = Arrests_2005.find_by_sql(["SELECT * FROM arrests_by_state_2005 WHERE arrests_by_state_2005.age = 'Total all ages' AND arrests_by_state_2005.state = ?", ss.name]);
+            drug_abuse = @a_2005.first.drug_abuse_violations.to_f
+          elsif ss.year==2007
+            @a_2007 = Arrests_2007.find_by_sql(["SELECT * FROM arrests_by_state_2007 WHERE arrests_by_state_2007.age = 'Total all ages' AND arrests_by_state_2007.state = ?", ss.name]);
+            drug_abuse = @a_2007.first.drug_abuse_violations.to_f
+          elsif ss.year==2009
+            @a_2009 = Arrests_2009.find_by_sql(["SELECT * FROM arrests_by_state_2009 WHERE arrests_by_state_2009.age = 'Total all ages' AND arrests_by_state_2009.state = ?", ss.name]);
+            drug_abuse = @a_2009.first.drug_abuse_violations.to_f
+          elsif ss.year==2011
+            @a_2011 = Arrests_2011.find_by_sql(["SELECT * FROM arrests_by_state_2011 WHERE arrests_by_state_2011.age = 'Total all ages' AND arrests_by_state_2011.state = ?", ss.name]);
+            drug_abuse = @a_2011.first.drug_abuse_violations.to_f
+          elsif ss.year==2012
+            @a_2012 = Arrests_2012.find_by_sql(["SELECT * FROM arrests_by_state_2012 WHERE arrests_by_state_2012.age = 'Total all ages' AND arrests_by_state_2012.state = ?", ss.name]);
+            drug_abuse = @a_2012.first.drug_abuse_violations.to_f
+          end    
+          percapita_drug1 = (drug_abuse/ss.est_pop)*100000                     
+          #Make a hash per crime type in format: {year1=>value1, year2=>value2}
+          @compare1_hash[ss.year] = percapita_drug1
+        end
+        States.where(name: params[:state2]).each do |ss|
+          if ss.year==2005
+            @a_2005 = Arrests_2005.find_by_sql(["SELECT * FROM arrests_by_state_2005 WHERE arrests_by_state_2005.age = 'Total all ages' AND arrests_by_state_2005.state = ?", ss.name]);
+            drug_abuse = @a_2005.first.drug_abuse_violations.to_f
+          elsif ss.year==2007
+            @a_2007 = Arrests_2007.find_by_sql(["SELECT * FROM arrests_by_state_2007 WHERE arrests_by_state_2007.age = 'Total all ages' AND arrests_by_state_2007.state = ?", ss.name]);
+            drug_abuse = @a_2007.first.drug_abuse_violations.to_f
+          elsif ss.year==2009
+            @a_2009 = Arrests_2009.find_by_sql(["SELECT * FROM arrests_by_state_2009 WHERE arrests_by_state_2009.age = 'Total all ages' AND arrests_by_state_2009.state = ?", ss.name]);
+            drug_abuse = @a_2009.first.drug_abuse_violations.to_f
+          elsif ss.year==2011
+            @a_2011 = Arrests_2011.find_by_sql(["SELECT * FROM arrests_by_state_2011 WHERE arrests_by_state_2011.age = 'Total all ages' AND arrests_by_state_2011.state = ?", ss.name]);
+            drug_abuse = @a_2011.first.drug_abuse_violations.to_f
+          elsif ss.year==2012
+            @a_2012 = Arrests_2012.find_by_sql(["SELECT * FROM arrests_by_state_2012 WHERE arrests_by_state_2012.age = 'Total all ages' AND arrests_by_state_2012.state = ?", ss.name]);
+            drug_abuse = @a_2012.first.drug_abuse_violations.to_f
+          end   
+          percapita_drug2 = (drug_abuse/ss.est_pop)*100000                     
+          #Make a hash per crime type in format: {year1=>value1, year2=>value2}
+          @compare2_hash[ss.year] = percapita_drug2
+        end
+    end
 	   render "_comparison_partial"
 	end
 
